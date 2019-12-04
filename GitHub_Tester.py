21de5a90b6865e825b05c8972ff3091b1825acfb
tbadeau4 <47260492+tbadeau4@users.noreply.github.com>
2019-12-04 09:11:21 -0700

    Add files via upload

---
+++ GitHub_Tester.py
@@ -0,0 +1,7 @@
+def github_tester():
+    #This function should print "Scratch and sniff, but it does not, please help me fix it!!
+    sentence = "I like scratch and sniff stickers."
+    print(sentence [8:24])
+
+
+    
