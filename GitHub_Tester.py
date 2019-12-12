3cbe35bea7d8cc8616647fb86b5b6ceb3acb93e4
SireSwag <SireSwag@users.noreply.github.com>
2019-12-12 08:11:54 -0700

    Update GitHub_Tester.py

--- GitHub_Tester.py
+++ GitHub_Tester.py
@@ -1,7 +1,5 @@
 def github_tester():
     #This function should print "Scratch and sniff, but it does not, please help me fix it!!
     sentence = "I like scratch and sniff stickers."
-    print(sentence [8:24])
-
-
-    
+    print(sentence [7:24]) #error was in string indice indexing
+github_tester()
