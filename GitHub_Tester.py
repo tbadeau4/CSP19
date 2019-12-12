5a459d6a6a338465943bf56ce088bf0940ce627f
MysteryGaming101 <58562450+MysteryGaming101@users.noreply.github.com>
2019-12-12 08:09:45 -0700

    Update GitHub_Tester.py
    Changed Index

--- GitHub_Tester.py
+++ GitHub_Tester.py
@@ -1,5 +1,5 @@
 def github_tester():
     #This function should print "Scratch and sniff, but it does not, please help me fix it!!
     sentence = "I like scratch and sniff stickers."
-    print(sentence [5:24])
+    print(sentence [7:24])
 
